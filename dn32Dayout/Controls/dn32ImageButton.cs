5cef4c0b8d24d42e2f3a992c5946751b8fe07940
Marcelo Vieira de Souza <dn@dn32.com.br>
2016-10-11 23:03:58 -0300

    Primeiro commit

---
+++ dn32Dayout/Controls/dn32ImageButton.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace dn32ImageButton.Controls
+{
+    public class dn32ImageButton : Button
+    {
+        static dn32ImageButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(dn32ImageButton), new FrameworkPropertyMetadata(typeof(dn32ImageButton)));
+        }
+
+        #region Dependency Properties
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(dn32ImageButton),
+            new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public double ImageSize
+        {
+            get { return (double)GetValue(ImageSizeProperty); }
+            set { SetValue(ImageSizeProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageSizeProperty =
+            DependencyProperty.Register("ImageSize", typeof(double), typeof(dn32ImageButton),
+            new FrameworkPropertyMetadata(30.0, FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public string NormalImage
+        {
+            get { return (string)GetValue(NormalImageProperty); }
+            set { SetValue(NormalImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty NormalImageProperty =
+            DependencyProperty.Register("NormalImage", typeof(string), typeof(dn32ImageButton),
+            new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.AffectsRender, ImageSourceChanged));
+
+        public string HoverImage
+        {
+            get { return (string)GetValue(HoverImageProperty); }
+            set { SetValue(HoverImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty HoverImageProperty =
+            DependencyProperty.Register("HoverImage", typeof(string), typeof(dn32ImageButton),
+            new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.AffectsRender, ImageSourceChanged));
+
+        public string PressedImage
+        {
+            get { return (string)GetValue(PressedImageProperty); }
+            set { SetValue(PressedImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty PressedImageProperty =
+            DependencyProperty.Register("PressedImage", typeof(string), typeof(dn32ImageButton),
+            new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.AffectsRender, ImageSourceChanged));
+
+        public string DisabledImage
+        {
+            get { return (string)GetValue(DisabledImageProperty); }
+            set { SetValue(DisabledImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty DisabledImageProperty =
+            DependencyProperty.Register("DisabledImage", typeof(string), typeof(dn32ImageButton),
+            new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.AffectsRender, ImageSourceChanged));
+
+        private static void ImageSourceChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            Application.GetResourceStream(new Uri("pack://application:,,," + (string)e.NewValue));
+        }
+
+        public Visibility BorderVisibility
+        {
+            get { return (Visibility)GetValue(BorderVisibilityProperty); }
+            set { SetValue(BorderVisibilityProperty, value); }
+        }
+
+        public static readonly DependencyProperty BorderVisibilityProperty =
+            DependencyProperty.Register("BorderVisibility", typeof(Visibility), typeof(dn32ImageButton),
+            new FrameworkPropertyMetadata(Visibility.Hidden, FrameworkPropertyMetadataOptions.AffectsRender));
+
+        #endregion
+    }
+}
